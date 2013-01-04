ae4f3f01a32e60ce88d623580bce47797b22336b
Florian Kasper <nirnana@khnetworks.com>
2013-01-04 20:45:39 +0100

    added book creation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,25 @@
 class BooksController < ApplicationController
+	def new
+		@authors = Author.where('id < 100')
+		@book = Book.new
+	end
+	def create
+		@newbook = Author.new(params[:book])
+		if @newbook.save
+			render 'new'
+		end
+	end
+	def show
+	
+	end
+	def index
+	
+	end
+	def destroy
+	
+	end
+	def edit
+	
+	end
+	
 end
