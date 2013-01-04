cf15f99585428b69b6d8e523cf4efc037f0ef12d
Florian Kasper <nirnana@khnetworks.com>
2013-01-04 23:46:12 +0100

    added named routes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,11 @@
 class SearchesController < ApplicationController
+	def search
+		@authors = Author.where("name like ?","%#{params[:keyword]}%")
+		@books = Book.where('name like ? or description like ? ',"%#{params[:keyword]}%","%#{params[:keyword]}%" )
+		
+		render 'display'
+	end
 	def index
+			redirect_to :action => :search, :keyword => params[:keyword]
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ RPO::Application.routes.draw do
   root to: 'splashes#show'
 
   resources :authors
+  match '/author/by-name/:author'			, to: 'authors#name'
+  
   resources :books
+  match '/book/by-name/:book'				, to: 'books#name'
   
   resources :searches
-  match '/search'							, to: 'searches#index', :via => :get
+  match '/search'							, to: 'searches#search', :via => :get
   
   resources :sessions						, only: [:new, :create, :destroy]
   match '/signin'							, to: 'sessions#create'
