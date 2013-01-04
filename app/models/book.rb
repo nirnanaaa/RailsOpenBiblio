9a2573678154bbbcd49df9aa3dc4bd6b99d7bbec
Florian Kasper <nirnana@khnetworks.com>
2013-01-05 00:01:51 +0100

    image validation and procession added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,17 @@
 class Book < ActiveRecord::Base
 	attr_accessible :name, :release, :kind, :isbn, :slogan, :author_id, :available, :image
 	
+	has_attached_file :image, 			:styles => 
+												{ :medium => "400x400",
+												  :thumb => "100x100#",
+												  :small => "150x150>"
+												},
+										:convert_options => {
+												  :thumb => "-quality 75 -strip" 
+															}
+
+	validates_attachment :image,		:content_type => { :content_type => "image/jpg" }, :size => {:in => 0..Settings.image.max_filesize.kilobytes}
+	
 	has_one :author
 	has_and_belongs_to_many :genres
   
