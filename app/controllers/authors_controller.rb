65e12fc26798b6d21021307da2918c844ebea1eb
Florian Kasper <nirnana@khnetworks.com>
2013-01-04 18:20:59 +0100

    added author creation

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,25 @@
 class AuthorsController < ApplicationController
+	before_filter :signed_in_filter
+	def create
+		@author = Author.new(params[:author])
+		
+		if @author.save
+			flash[:success] = "Author #{@author.name} created"
+		else
+			flash[:error] = "Could not create author #{@author.name}"
+			render 'new'
+		end
+	end
+	def index
+	
+	end
+	def show
+	
+	end
+	def destroy
+	
+	end
+	def edit
+	
+	end
 end
