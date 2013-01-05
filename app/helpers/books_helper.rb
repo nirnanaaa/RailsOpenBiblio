4ea8454ff18883a80e70d2e8090b8fddf4755839
Florian Kasper <nirnana@khnetworks.com>
2013-01-05 18:31:54 +0100

    added &nbsp; remover

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,9 +1,9 @@
 module BooksHelper
 	def tag_generator(list)
-		splitted = list.split(',')
+		splitted = list.split(" ").join("").split(',')
 		new = Array.new
 		splitted.each do |s|
-			new << [ link_to(s.capitalize, tags_path(s.downcase)) ]
+			new << [ link_to(s.capitalize, "/tag/#{s.downcase}") ]
 		end
 		new.join
 	end
