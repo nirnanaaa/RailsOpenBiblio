c9910ab7420e803f27e8c2b0b99669e9e4104e6e
Florian Kasper <nirnana@khnetworks.com>
2013-01-05 18:29:12 +0100

    added some useful routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,19 @@ RPO::Application.routes.draw do
   root :to => 'splashes#show'
 
   resources :tags
-  match '/tag/:tag'							, to: 'tags#show', :via => :get
+  match '/tag/:tag'							, to: 'tags#show'		, :via => :get
   
   resources :authors
-  match '/author/:author'					, to: 'authors#name'
+  match '/author/:author'					, to: 'authors#name'	, :via => :get
   
   resources :books
-  match '/book/:book'						, to: 'books#name'
+  match '/book/:book'						, to: 'books#name'		, :via => :get
+  
+  match '/user/:name'						, to: 'users#show'		, :via => :get
+  match '/user/:name/edit'					, to: 'users#edit'		, :via => :get
   
   resources :searches
-  match '/search'							, to: 'searches#search', :via => :get
+  match '/search'							, to: 'searches#search'	, :via => :get
   
   resources :sessions						, only: [:new, :create, :destroy]
   match '/signin'							, to: 'sessions#create'
