48786e37bb5c0019e0f1a17c77ecda768f6750b4
Florian Kasper <nirnana@khnetworks.com>
2013-01-06 16:38:45 +0100

    allow nil on born and died

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,14 +1,32 @@
 class Author < ActiveRecord::Base
-	attr_accessible :name, :biography, :born, :died, :avatar
-	
-	has_attached_file :avatar, 			:styles => { :medium => "400x400>", :thumb => "100x100>" }
+  
+	attr_accessible                :name, 
+	                               :biography, 
+	                               :born, 
+	                               :died, 
+	                               :avatar
 
-	validates_attachment :avatar,		:size => {:in => 0..Settings.image.max_filesize.kilobytes}
+	has_attached_file :avatar, 			:styles => { 
+	                                             :medium => "400x400>", 
+	                                             :thumb => "100x100>",
+	                                             :small => "",
+	                                             :large => "" 
+	                                           }
+
+	validates_attachment :avatar,		:size => {
+	                                             :in => 0..Settings.image.max_filesize.kilobytes
+	                                         }
 	
-	validates :name						, :presence => true, :uniqueness => true
 	
-	validates :born						, :numericality => { :only_integer => true}
-	validates :died						, :numericality => { :only_integer => true}
+	validates :name						    , :uniqueness => true,
+	                                :on => :create
+	                                
+  validates :name               , :presence => true,
+                                  :on => :save
+                                
+
+	validates :born						, :numericality => { :only_integer => true, :allow_nil => true}
+	validates :died						, :numericality => { :only_integer => true, :allow_nil => true}
 	
 	has_many :books
 	validates_associated :books
