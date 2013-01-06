6b81b8edf384fb3db250c180a9c59df3cd46300c
Florian Kasper <nirnana@khnetworks.com>
2013-01-06 11:09:18 +0100

    refactored some tabs

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,16 +3,25 @@ class Book < ActiveRecord::Base
 	##Add Image to /new!
 	##
 	##
-	attr_accessible :name, :release, :kind, :isbn, :slogan, :author_id, :available, :image, :fullname, :description
+	attr_accessible :name,
+	                :release,
+	                :kind, 
+	                :isbn, 
+	                :slogan, 
+	                :author_id, 
+	                :available, 
+	                :image, 
+	                :fullname, 
+	                :description,
+	                :downloadable
 
-	has_attached_file :image, 			:styles => 
-												{ :medium => Settings.image.medium,
-												  :thumb => Settings.image.thumb,
-												  :small => Settings.image.small
-												},
-										:convert_options => {
-												  :thumb => "-quality #{Settings.image.thumb_quality} -strip" 
-															}
+	has_attached_file :image, 			:styles => { :medium => Settings.image.medium,
+                                               :thumb => Settings.image.thumb,
+                                               :small => Settings.image.small
+												                     },
+										              :convert_options => {
+												                       :thumb => "-quality #{Settings.image.thumb_quality} -strip" 
+															                        }
 
 	validates_attachment :image,		:size => {:in => 0..Settings.image.max_filesize.kilobytes}
 	validates_length_of :isbn,			:in => 9..13, :allow_nil => true
