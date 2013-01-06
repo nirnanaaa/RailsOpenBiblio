cd7c8a0a7c15320c12f073739b135569400071eb
Florian Kasper <nirnana@khnetworks.com>
2013-01-06 13:38:32 +0100

    finished for today

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,15 @@ class TagsController < ApplicationController
 	def show
 		render :text => params[:tag]
 	end
+	def new
+	  
+	end
+	def create
+	  
+	end
+	def destroy
+	  
+	end
 	def index
 	  
 	end
