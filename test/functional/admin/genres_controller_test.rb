bbf11eef41cadaceb304e0d1692e8d758da10a85
nirnanaaa <nirnanaaa@khnetworks.com>
2013-01-12 18:33:13 +0100

    added some admin modules

---
+++ test/functional/admin/genres_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Admin::GenresControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/unit/helpers/admin/genres_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Admin::GenresHelperTest < ActionView::TestCase
+end
